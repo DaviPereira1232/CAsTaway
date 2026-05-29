806d5dd3c79b3e05b3810e7e46a13eed1d0ad217
Diana <dfranciscamagalhaes@gmail.com>
2026-05-29 12:44:52 +0100

    Audio Mixer Adicionado

---
+++ Assets/Menus_Diana&Joana/Script/VolumeSlider.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class VolumeSlider : MonoBehaviour
+{
+    [SerializeField] 
+    private AudioMixer audioMixer;
+
+    [SerializeField]
+    private Slider volumeSlider;
+
+    public void Start()
+    {
+        if (PlayerPrefs.HasKey("MusicVolume"))
+        {
+            LoadVolume();
+        }
+        else
+        {
+        SetMusicVolume();
+        }
+    }
+
+    public void SetMusicVolume()
+    {
+        float volume = volumeSlider.value;
+        audioMixer.SetFloat("Music", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("MusicVolume", volume);
+    }
+
+    private void LoadVolume()
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat("MusicVolume");
+
+        SetMusicVolume();
+    }
+}
