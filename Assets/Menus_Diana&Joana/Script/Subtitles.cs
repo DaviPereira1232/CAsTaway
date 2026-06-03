2adad8476cd9f6bfbfb37661fa18a80c5b56572a
Diana <dfranciscamagalhaes@gmail.com>
2026-06-03 09:09:23 +0100

    mais ui

--- Assets/Menus_Diana&Joana/Script/Subtitles.cs
+++ Assets/Menus_Diana&Joana/Script/Subtitles.cs
@@ -1,16 +1,30 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Subtitles : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Button leftArrow;
+    public Button rightArrow;
+    private TMP_Text subtitleText;  // Still need TMP_Text
+    
     void Start()
     {
+        // This works if the script is on the SAME GameObject as the TextMeshPro component
+        subtitleText = GetComponent<TMP_Text>();
         
+        if (subtitleText == null)
+        {
+            Debug.LogError("No TMP_Text component found on this GameObject!");
+            return;
+        }
+        
+        leftArrow.onClick.AddListener(() => SwitchText("English"));
+        rightArrow.onClick.AddListener(() => SwitchText("Português"));
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    void SwitchText(string text)
     {
-        
+        subtitleText.text = text;
     }
 }
