b45f67d7177d3f2ca22f5dc91fa4477a8443ec56
Diana <dfranciscamagalhaes@gmail.com>
2026-06-22 20:54:47 +0100

    Trying to set up a game manager

--- Assets/Menus_Diana&Joana/Script/GameManager.cs
+++ Assets/Menus_Diana&Joana/Script/GameManager.cs
@@ -3,25 +3,4 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager instance { get; private set; }
-
-    // References to your settings scripts (will be assigned in each scene)
-    public VolumeSlider volumeSlider;
-    public ContrastSlider contrastSlider;
-    public ColorBlindness colorBlindness;
-    public ZoomSettings zoomSettings;
-    public ControlsManager controlsManager;
-
-    void Awake()
-    {
-        // Singleton pattern - only one GameManager exists
-        if (instance != null && instance != this)
-        {
-            Destroy(gameObject);
-            return;
-        }
-
-        instance = this;
-        DontDestroyOnLoad(gameObject);
-    }
 }
