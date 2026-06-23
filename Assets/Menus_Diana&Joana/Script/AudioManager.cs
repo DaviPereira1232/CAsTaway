1b6640f22cea621f77faf836415411bc8fc821f0
Diana <dfranciscamagalhaes@gmail.com>
2026-06-23 16:45:25 +0100

    Working on Audio

--- Assets/Menus_Diana&Joana/Script/AudioManager.cs
+++ Assets/Menus_Diana&Joana/Script/AudioManager.cs
@@ -1,19 +1,52 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class AudioManager : MonoBehaviour
 {
-    [Header("-----------Audio Sources-----------")]
+    public static AudioManager Instance;
+
+    [Header("----Audio Sources----")]
     [SerializeField] private AudioSource musicSource;
-    [SerializeField] private AudioSource sfxSource;
+    //[SerializeField] private AudioSource sfxSource;
 
-    [Header("-----------Audio Clips-----------")]
+    [Header("----SFX Pool----")]
+    //[SerializeField] private GameObject sfxSourcePrefab;
+    //[SerializeField] private int sfxPoolSize =
+    [Header("----Audio Clips----")]
 
-    //[SerializeField] private AudioClip SFX;
     [SerializeField] private AudioClip background;
+
+    [Header("Level Audio")]
+
     [SerializeField] private AudioClip damage;
     [SerializeField] private AudioClip bubbles;
-    [SerializeField] private AudioClip seaurchin;
-    [SerializeField] private AudioClip finishlevel;
+    [SerializeField] private AudioClip waterswosh;
+    [SerializeField] private AudioClip success;
+    [SerializeField] private AudioClip partialSucess;
+    [SerializeField] private AudioClip failure;
+    [SerializeField] private AudioClip scaredCat;
+    [SerializeField] private AudioClip happyCat;
+    [SerializeField] private AudioClip unwellCat;
+
+    [Header("Main Menu Audio")]
+
+    [SerializeField] private AudioClip computer;
+    [SerializeField] private AudioClip exitingSpaceship;
+    [SerializeField] private AudioClip catSleeping;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
 
     void Start()
     {
